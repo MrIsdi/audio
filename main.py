c0462e94c1e10ed322cbd7fe094a1dd3f85edbec
MrIsdi <isdiridho@gmail.com>
2024-04-30 12:00:38 +0700

    fix: main.py

--- main.py
+++ main.py
@@ -7,29 +7,29 @@ base_dir = "dataset"
 if not os.path.exists(base_dir):
     os.mkdir("dataset")
     
-buka_dir = os.path.join(base_dir, "buka")
-tutup_dir = os.path.join(base_dir, "tutup")
+train_dir = os.path.join(base_dir, "train")
+val_dir = os.path.join(base_dir, "val")
 
-if not os.path.exists(buka_dir):
-    os.mkdir(buka_dir)
-if not os.path.exists(tutup_dir):
-    os.mkdir(tutup_dir)
+if not os.path.exists(train_dir):
+    os.mkdir(train_dir)
+if not os.path.exists(val_dir):
+    os.mkdir(val_dir)
     
-buka_train_dir = os.path.join(buka_dir, "train")
-buka_val_dir = os.path.join(buka_dir, "val")
+train_buka_dir = os.path.join(train_dir, "buka")
+train_tutup_dir = os.path.join(train_dir, "tutup")
 
-if not os.path.exists(buka_train_dir):
-    os.mkdir(buka_train_dir)
-if not os.path.exists(buka_val_dir):
-    os.mkdir(buka_val_dir)
+if not os.path.exists(train_buka_dir):
+    os.mkdir(train_buka_dir)
+if not os.path.exists(train_tutup_dir):
+    os.mkdir(train_tutup_dir)
 
-tutup_train_dir = os.path.join(tutup_dir, "train")
-tutup_val_dir = os.path.join(tutup_dir, "val")
+val_buka_dir = os.path.join(val_dir, "buka")
+val_tutup_dir = os.path.join(val_dir, "tutup")
 
-if not os.path.exists(tutup_train_dir):
-    os.mkdir(tutup_train_dir)
-if not os.path.exists(tutup_val_dir):
-    os.mkdir(tutup_val_dir)
+if not os.path.exists(val_buka_dir):
+    os.mkdir(val_buka_dir)
+if not os.path.exists(val_tutup_dir):
+    os.mkdir(val_tutup_dir)
     
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
@@ -41,16 +41,16 @@ def make_dataset_sound(voices, text, dir, total_data):
             engine.setProperty('rate', rate[i])
             engine.setProperty('volume', random.uniform(0,1))
             engine.setProperty('voice', voices[int(np.round(random.uniform(0,1)))].id)
-            engine.save_to_file(f"{text}", f"{dir}/train/{text}_{i}.wav")
+            engine.save_to_file(f"{text}", f"{dir}/train/{text}/{text}_{i}.wav")
         else:
             engine.setProperty('rate', rate[i])
             engine.setProperty('volume', random.uniform(0,1))
             engine.setProperty('voice', voices[int(np.round(random.uniform(0,1)))].id)
-            engine.save_to_file(f"{text}", f"{dir}/val/{text}_{i}.wav")
+            engine.save_to_file(f"{text}", f"{dir}/val/{text}/{text}_{i}.wav")
         engine.runAndWait()
 
 if __name__ == "__main__":
     print("Dataset mulai dibuat")
-    make_dataset_sound(voices, "buka", buka_dir, 200)
-    make_dataset_sound(voices, "tutup", tutup_dir, 200)
+    make_dataset_sound(voices, "buka", base_dir, 200)
+    make_dataset_sound(voices, "tutup", base_dir, 200)
     print("Dataset selesai dibuat")
